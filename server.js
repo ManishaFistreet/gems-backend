c159b30a2e7e8a64fb09613e0d16c04acf8fca99
ManishaFistreet <manisha@fistreet.in>
2025-07-31 16:19:08 +0530

    apis changes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const productRoutes = require("./routes/productRoutes");
 const itemRoutes = require("./controllers/itemController");
 const salesItemRoutes = require("./routes/salesRoutes");
 const customerItemRoutes = require("./routes/customerRoute");
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -22,7 +23,7 @@ mongoose
 
 app.use("/api/products", productRoutes);
 app.use("/api/items", itemRoutes);
-
+app.use('/api/auth', authRoutes);
 app.use("/api/sales", salesItemRoutes);       
 app.use("/api/customers", customerItemRoutes);
 
